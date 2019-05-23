0b36d32d16040ef723f44e70023093e1cfec7cc2
Lei Nelissen <lei@codified.nl>
2019-05-23 09:23:09 +0200

    Add the Miep algorithm

--- app/src/screens/Dashboard/components/Message.js
+++ app/src/screens/Dashboard/components/Message.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { Colors } from '../../../styles';
 
 class Message extends Component {
@@ -10,9 +10,47 @@ class Message extends Component {
             windowIsOpen,
             windowIsClosed
         } = this.props;
+
+        if (temperature < 18 && co2 < 1000) {
+            return "All doors and windows should be closed";
+        } else if (temperature < 20 && co2 < 1000) {
+            return "It might be good to close all doors and window";
+        } else if (temperature < 22 && co2 < 1000) {
+            return "A window should be opened";
+        } else if (temperature > 22 && co2 < 1000) {
+            return "You should open the window and all indoor doors";
+        } else if (temperature < 18 && co2 < 2000) {
+            return "Try to open the doors to other rooms";
+        } else if (temperature < 20 && co2 < 2000) {
+            return "The window should be openend";
+        } else if (temperature < 22 && co2 < 2000) {
+            return "The window should be opened";
+        } else if (temperature > 22 && co2 < 2000) {
+            return "Both the door and window should be opened";
+        } else if (temperature < 18 && co2 < 5000) {
+            return "Open the inside doors and the kitchen window";
+        } else if (temperature < 20 && co2 < 5000) {
+            return "Open the door to outside";
+        } else if (temperature < 22 && co2 < 5000) {
+            return "Both the door and the window should be open";
+        } else if (temperature > 22 && co2 < 5000) {
+            return "Your inside air quality isn't looking good! Try to open as much as possible";
+        } else if (temperature < 18 && co2 > 5000) {
+            return "You might want to open a window to avoid a headache";
+        } else if (temperature < 20 && co2 > 5000) {
+            return "You have a CO2 problem! Both the window and door should be open";
+        } else if (temperature < 22 && co2 > 5000) {
+            return "You have a CO2 problem! Both the window and door should be open";
+        } else if (temperature > 22 && co2 > 5000) {
+            return "Your inside air quality is REALLY, REALLY bad! Open everything! You should also close the blinds";
+        }
     }
 
     render() {
+        if (!this.props.co2 || !this.props.temperature) {
+            return <ActivityIndicator />;
+        }
+
         return (
             <View style={styles.container}>
                 <Text style={styles.text}>Miep says: "{this.getMiepMessage()}"</Text>
